92ccc8dd7fc03f878737a760fb01540c7a3ab4ba
hickmb34@gmail.com <hickmb34@gmail.com>
2025-09-29 06:05:33 +0000

    Created Base.urls and CourseUnlocks; Edited Base and MyCourses

---
+++ server_code/CourseUnlocks.py
@@ -0,0 +1,23 @@
+import anvil.stripe
+import anvil.google.auth, anvil.google.drive, anvil.google.mail
+from anvil.google.drive import app_files
+import anvil.facebook.auth
+import anvil.users
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.server
+
+# This is a server module. It runs on the Anvil server,
+# rather than in the user's browser.
+@anvil.server.callable
+def get_my_courses():
+  user = anvil.users.get_user()
+  if user == None:
+    return []
+
+  if not user["purchased_courses"]:
+    course_info = app_tables.courses.get(id_name=course)
+    courses.append(course_info)
+
+    return courses
