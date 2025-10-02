28322187da546dc173ad74b0de35c2eed800e3d5
hickmb34@gmail.com <hickmb34@gmail.com>
2025-10-02 02:09:51 +0000

    Created Base.urls and CourseUnlocks; Edited Base and MyCourses

--- client_code/MyCourses/__init__.py
+++ client_code/MyCourses/__init__.py
@@ -9,11 +9,29 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 import anvil.users
+from ..CourseItem import CourseItem
 
 
 class MyCourses(MyCoursesTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    self.load_courses()
 
     # Any code you write here will run before the form opens.
+  def render_course(self, course_name):
+    self.content_panel.clear
+
+  def load_courses(self):
+    courses= anvil.server.call("get_my_courses")
+
+    if len(courses)> 0:
+      self.no_courses_label.visible = False
+
+      course_panel = GridPanel()
+
+      for i, course in enumerate(courses):
+        c = CourseItem(name=course["name"], button_text="ViewContent", description= course["description"], image= course["image"], button_callback= self.render_course)
+        course_panel.add_component(c, row=str(i//2), width_xs=6)
+
+      self.content_panel.add_component(course_panel)
